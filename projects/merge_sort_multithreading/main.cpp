f9a38351ee13c85de8e229b1103033f50e520dd1
ItsKarlito <karlnoory@hotmail.com>
2021-02-03 23:00:58 -0500

    added file i/o

--- projects/merge_sort_multithreading/main.cpp
+++ projects/merge_sort_multithreading/main.cpp
@@ -1,20 +1,51 @@
 #include "merge_sort.hpp"
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+#define FILE_DIR "input.txt"
+#define FILE_ERROR "ERROR: Could not read from file"
 
 int main(int argc, char const *argv[])
 {
-    MergeSort<int> sort;
-    int test[] = {2,34,45,2,8,7,5,98,6};
-
-    int size = sizeof(test)/sizeof(int);
-    sort.sort(test, size);
-
-    for(int i = 0; i < size; i++)
+    // open file, check if successful
+    std::ifstream file(FILE_DIR);
+    if (!file.is_open())
     {
-        std::cout << test[i] << ", ";
+        std::cout << FILE_ERROR << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // read file, store integers in vector, close file
+    std::vector<int> integerVector;
+    int value;
+    while (file >> value)
+    {
+        integerVector.push_back(value);
+    }
+    file.close();
+
+    // create array from vector
+    int integerArray[integerVector.size()] = {};
+    for (int i = 0; i < integerVector.size(); i++)
+    {
+        integerArray[i] = integerVector[i];
+    }
+    std::vector<int>().swap(integerVector); // free up memory
+
+    // call threaded mergesort
+    MergeSort<int> sort;
+
+    int size = sizeof(integerArray) / sizeof(int);
+
+    sort.sort(integerArray, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << integerArray[i] << ", ";
     }
     std::cout << "\n";
 
-    return 0;
+    return EXIT_SUCCESS;
 }
