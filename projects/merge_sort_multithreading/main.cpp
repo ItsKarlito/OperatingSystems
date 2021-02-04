70e8c7112ccda64240ce16ae8f514fe57ab82358
ItsKarlito <karlnoory@hotmail.com>
2021-02-04 14:27:54 -0500

    improved file i/o logic

--- projects/merge_sort_multithreading/main.cpp
+++ projects/merge_sort_multithreading/main.cpp
@@ -4,27 +4,22 @@
 #include <fstream>
 #include <vector>
 
-#define FILE_DIR "input.txt"
-#define FILE_ERROR "ERROR: Could not read from file"
-
 //argc = argument count
 //argv = argument verbose
 int main(int argc, char const *argv[])
 {
-    // open file, check if successful
-    std::ifstream file;
-    if(argc == 2)
+    //Process CLI arguments, open file, check if successful
+    std::ifstream file("input.txt");
+    if (argc == 2)
         file = std::ifstream(argv[1]);
-    else
-        file = std::ifstream(FILE_DIR);
-    
+
     if (!file.is_open())
     {
-        std::cout << FILE_ERROR << std::endl;
+        std::cout << "ERROR: Could not read from file" << std::endl;
         return EXIT_FAILURE;
     }
 
-    // read file, store integers in vector, close file
+    //Read file, store integers in vector, close file
     std::vector<int> integerVector;
     int value;
     while (file >> value)
@@ -33,13 +28,15 @@ int main(int argc, char const *argv[])
     }
     file.close();
 
-    // call threaded mergesort
+    //Call threaded mergesort
     MergeSort<int> sort;
 
     sort.sort(integerVector.data(), integerVector.size());
 
     for (int i = 0; i < integerVector.size(); i++)
-    { std::cout << integerVector[i] << ", "; }
+    {
+        std::cout << integerVector[i] << ", ";
+    }
     std::cout << "\n";
 
     return EXIT_SUCCESS;
--- projects/merge_sort_multithreading/merge_sort.hpp
+++ projects/merge_sort_multithreading/merge_sort.hpp
@@ -10,42 +10,41 @@ template <typename Type>
 class MergeSort
 {
 public:
-    MergeSort(){}
+    MergeSort() {}
 
     void sort(Type *arr, size_t size)
     {
-        if(size > 0)
-            sort(arr, 0, size-1);
+        if (size > 0)
+            sort(arr, 0, size - 1);
         else
-            throw std::runtime_error("sorting size cannot be lower than 1");
-    } 
+            throw std::runtime_error("ERROR: Sorting size cannot be lower than 1");
+    }
 
 private:
     //Sort for specific segment of array
     void sort(Type *arr, int start, int end)
     {
         //Check if arr is divisible
-        if(start >= end)
+        if (start >= end)
             return;
 
         //Divide arr into two segments, and sort each
-        int m = start + (end - start)/2;
+        int m = start + (end - start) / 2;
 
         //DON'T FORGET TO JOIN THIS TWO THREADS NO MATTER WHAT
         sort(arr, start, m);
-        sort(arr, m+1, end);
-        
+        sort(arr, m + 1, end);
+
         //Merge both segments into one. The end result will
         //be on the same location as arr
-        merge(arr, start, m, m+1, end);
+        merge(arr, start, m, m + 1, end);
     }
 
     //Merge two segments into arr
     void merge(
         Type *arr,
-        int start_l, int end_l, 
-        int start_r, int end_r
-    )
+        int start_l, int end_l,
+        int start_r, int end_r)
     {
         //left segment       right segment
         //[start_l ... end_l][start_r ... end_r]
@@ -66,15 +65,15 @@ private:
         //into arr_l and arr_r respectively
         int arr_l[size_l] = {0};
         int arr_r[size_r] = {0};
-        memcpy(arr_l, &arr[start_l], size_l*sizeof(Type));
-        memcpy(arr_r, &arr[start_r], size_r*sizeof(Type));
+        memcpy(arr_l, &arr[start_l], size_l * sizeof(Type));
+        memcpy(arr_r, &arr[start_r], size_r * sizeof(Type));
 
-        while(i_l < size_l && i_r < size_r)
+        while (i_l < size_l && i_r < size_r)
         {
             //If current element on the left array
             //is bigger than the current element on
             //the right one, copy it on arr and increment i_l
-            if(arr_l[i_l] <= arr_r[i_r])
+            if (arr_l[i_l] <= arr_r[i_r])
                 arr[i] = arr_l[i_l++];
             //Otherwise, do the same but with arr_r and i_r
             else
@@ -85,7 +84,7 @@ private:
         }
 
         //Make sure there are no data left to be added
-        while (i_l < size_l) 
+        while (i_l < size_l)
             arr[i++] = arr_l[i_l++];
         while (i_r < size_r)
             arr[i++] = arr_r[i_r++];
