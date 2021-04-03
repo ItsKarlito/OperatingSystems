740c3ffc9021a7b0fad0a356adf42bf4b1ef5592
ItsKarlito <karlnoory@hotmail.com>
2021-04-03 19:24:39 -0400

    default path, return flag, mem config test

--- projects/virtual_memory_manager/main.cpp
+++ projects/virtual_memory_manager/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char const *argv[])
 {
-    std::string inputFilePath = "/projects/fair_share_process_scheduling/";
+    std::string inputFilePath = "./projects/virtual_memory_manager/";
     if(argc == 2)
     {
         inputFilePath = (char *)argv[1];
@@ -22,12 +22,14 @@ int main(int argc, char const *argv[])
     }
     catch(char const* e)
     {
-        std::cout << "Error: " << e << '\n';
+        std::cout << e << '\n';
+        return EXIT_FAILURE;
     }
 
     Parser::processData *pData = parser.getProcessData();
     Parser::cmdData *cData = parser.getCmdData();
 
+    std::cout << "MEM DATA: \n  Number of pages: " << numPages << "\n\n";
     parser.printProcessData();
     cData->printCmdData();
 
