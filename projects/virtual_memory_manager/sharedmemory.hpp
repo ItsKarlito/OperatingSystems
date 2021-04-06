871343a56c01f92ce75dd9beaca4a3abe0f45074
ItsKarlito <karlnoory@hotmail.com>
2021-04-05 23:50:43 -0400

    added SharedMemory class

--- projects/virtual_memory_manager/sharedmemory.hpp
+++ projects/virtual_memory_manager/sharedmemory.hpp
@@ -1,12 +1,36 @@
 #ifndef SHAREDMEMORY_HPP_
 #define SHAREDMEMORY_HPP_
 
+#include <queue>
+#include <mutex>
+#include "parser.hpp"
+
 class SharedMemory
 {
-public:
-
 private:
+    std::mutex commands_mtx;
+    std::queue<Parser::Command> commands;
+
+public:
     SharedMemory() {}
+
+    void pushCmd(Parser::Command cmd)
+    {
+        commands_mtx.lock();
+        commands.push(cmd);
+        commands_mtx.unlock();
+    }
+
+    Parser::Command popCmd()
+    {
+        commands_mtx.lock();
+        if (commands.size() > 0)
+        {
+            commands.pop();
+        }
+        commands_mtx.unlock();
+    }
+
     ~SharedMemory() {}
 };
 
