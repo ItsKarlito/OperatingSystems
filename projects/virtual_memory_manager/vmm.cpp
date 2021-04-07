8a2976fbf4546e6e83487363be9af9cd2903b931
thelastbilly <jtmonegro@gmail.com>
2021-04-06 21:07:54 -0400

    added comments and accessors

--- projects/virtual_memory_manager/vmm.cpp
+++ projects/virtual_memory_manager/vmm.cpp
@@ -4,11 +4,15 @@ namespace vmm
 {
     void vmem_f::write(const std::string& path)
     {
+        // Open file, or throw an exception if you can't open it
         std::ofstream f(path, std::ofstream::out | std::ofstream::binary);
         if(!f.is_open())
             throw std::runtime_error("cannot write to \"" + path + "\"");
 
+        // Allocate some memory space for the file data
         char* buffer = (char*)malloc(sizeof(page_t)*pages.size() + sizeof(char));
+
+        // Write this->pages info into the array
         size_t index = 0;
         for(const page_t& page : this->pages)
         {
@@ -16,34 +20,51 @@ namespace vmm
             memcpy((void *)&buffer[index], (void *)&page, sizeof(page_t));
             index += sizeof(page_t);
         }
+
+        // Write array into file
         f.write(buffer, index+1);
+
+        // Free array and close file
         free((void*)buffer);
         f.close();
     }
 
     void vmem_f::read(const std::string& path)
     {
+        // Open file for reading or throw an exception if you can't. Also, reset the file pointer to
+        // the end to get the size
         std::ifstream f(path, std::ifstream::in | std::ifstream::binary | std::ifstream::ate);
         if(!f.is_open())
             throw std::runtime_error("cannot read \"" + path + "\"");
 
+        // Delete previous pages entries
         this->pages.clear();
 
+        // Get the file size with the file pointer
         size_t file_size = f.tellg();
+
+        // Reset the file pointer to the beggining of the file
         f.clear();
         f.seekg(0, std::ifstream::beg);
-
+        
+        // Check if the file has a size larger than one
         if(file_size < 1)
         {
             f.close();
             return;
         }
 
+        // Read the file contents into memory (buffer), and close the file since
+        // we will be no longer using it
         std::string buffer;
         buffer.reserve(file_size);        
         buffer.assign((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
         f.close();
 
+        // Go through the file. If you find a ',' and you did so after
+        // sizeof(page_t)+1 amount for bytes has passed, copy the bytes
+        // in between the current and last ',' into a page_t object. Then
+        // append that object into this->pages.
         std::string s_b = "";
         page_t t = {};
         size_t safe = 0;
--- projects/virtual_memory_manager/vmm.hpp
+++ projects/virtual_memory_manager/vmm.hpp
@@ -10,33 +10,42 @@
 
 namespace vmm
 {
-    static const char FILE_TAG = ',';
+    static const char FILE_TAG = ',';                                   // Used to mark limits in virtual memory file
     typedef unsigned int uint;
-    struct page_t
+    struct page_t                                                       // Holds page info
     {
         int val;
         int id;
         uint last_access_time;
 
-        operator std::string() const {
+        operator std::string() const {                                  // Mostly for debugging. Used to print the values of page_t
             std::stringstream ss;
             ss << id << ", " << val << ", " << last_access_time;
             return ss.str();
         }
     };
+
     class vmem_f
     {
     private:
         typedef std::vector<page_t> pages_t;
     public:
-        vmem_f() {}
+        vmem_f() {}                                                     // Default constructor
+        vmem_f(const vmem_f& v) { this->pages = v.pages; }              // Copy contructor
         
-        void read(const std::string& path);
-        void write(const std::string& path);
+        void read(const std::string& path);                             // Parses file in path and writes results to this->pages
+        void write(const std::string& path);                            // Writes the contents of this->pages into a file
 
-        pages_t& get_pages() { return this->pages; }
+        pages_t& get_pages() { return this->pages; }                    // Get reference to this->pages vector
+
+        void add_page( const page_t& page )                             // Adds page to pages
+            { this->pages.push_back(page); }
+        page_t& retrieve_page( size_t i )                               // Retrieve page at index
+            { return this->pages.at(i); }
+        size_t size()                                                   // Get pages size
+            { return this->pages.size(); }
     private:
-        std::vector<page_t> pages;
+        std::vector<page_t> pages;                                      // Stores parsed/defined pages
     };
 }
 
